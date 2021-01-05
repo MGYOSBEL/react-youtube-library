3341ecb753decba780ffb26ac8fa08bb71a335d6
Ernesto Alejandro Diaz Catala <ernestoalejandro.diazcatala@grandparade.co.uk>
2021-01-05 15:30:12 +0100

    feat: improve backdrop

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Backdrop = ({ show, clicked }) =>
-  show ? (
-    <div
-      className="fixed w-screen h-screen left-0 top-0 bg-black bg-opacity-50 z-10"
-      onClick={clicked}
-      onKeyDown={clicked}
-      role="button"
-      tabIndex="0"
-    ></div>
-  ) : null;
+import Transition from '../../../Transition';
+
+const Backdrop = ({ show, clicked }) => {
+  return (
+    <>
+      <Transition
+        show={show}
+        enter="transition ease-out duration-100 transform"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="transition ease-in duration-75 transform"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        <div
+          className="fixed w-screen h-screen left-0 top-0 bg-black bg-opacity-50 z-10"
+          onClick={clicked}
+          onKeyDown={clicked}
+          role="button"
+          tabIndex="0"
+        ></div>
+      </Transition>
+      <div></div>
+    </>
+  );
+};
 
 export default Backdrop;
 
